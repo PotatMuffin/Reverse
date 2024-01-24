6601399263182d4f0f38d421a57213ec6f2180e7
potatmuffin <henk8017@gmail.com>
2024-01-24 20:15:31 +0000

    initial commit

---
+++ src/Reverse.asm
@@ -0,0 +1,52 @@
+format ELF64 executable
+
+segment readable executable
+
+include "system.inc"
+include "tokenizer.inc"
+include "parser.inc"
+entry $
+
+check_param_count:
+    mov rax, [rsp]
+    cmp rax, 2
+    jge get_params
+
+    printmany usage_suffix, [rsp + 8], usage
+    exit_process 1
+
+get_params:
+    mov rbx, [rsp + 16]
+    mov [source], rbx
+
+main:
+    mov rbp, rsp
+    sub rsp, 24
+
+    call init_memory
+    
+    open [source], 0, 0
+    mov [rbp - 8], rax
+    
+    cmp rax, 0
+    jl file_error
+
+    mov rdi, [rbp - 8] 
+    call read_file
+
+    mov [rbp - 16], rax
+
+    close [rbp - 8]
+
+    mov rdi, [rbp - 16]
+    call tokenize
+    mov [rbp - 24], rax
+    
+    mov rdi, [rbp - 24]
+    call parse
+
+    exit_process 0
+
+segment readable writable
+include "variables.inc"
+include "errors.inc"
